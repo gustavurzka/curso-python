702c849e2fbdaca6a090ee8164c2b1afe5da8da3
gustavurzka <gustavo_henrique199@live.com>
2022-07-25 09:51:47 -0300

    Teste

---
+++ aula4/aula4.py
@@ -0,0 +1,5 @@
+print('Gustavo', type('Gustavo'))
+print(23, type(23))
+print(1.80, type(1.80))
+print(23 >= 17, type(23 >= 17))
+
---
+++ aula6/aula6..py
@@ -0,0 +1,10 @@
+#  Aula de váriáveis
+nome = 'Gustavo'
+idade = 23
+altura = 1.80
+e_maior = idade > 18
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)

4775a197bac1549b732ea345208fca8efaaaad13
gustavurzka <gustavo_henrique199@live.com>
2022-07-28 15:54:01 -0300

    Aula 9

--- aula9/aula9.py
+++ aula9/aula9.py
@@ -1,3 +1,11 @@
 # IF,ELSE e ELIF
 num1 = input('Digite um número ')
 num2 = input('Digite o segundo número')
+
+try:
+    num1 = int(num1)
+    num2 = int(num2)
+
+    print(num1 + num2)
+except:
+    print('ERROR')

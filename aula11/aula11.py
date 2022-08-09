291bb30f715a4bf37613417427415f37610e5148
gustavurzka <gustavo_henrique199@live.com>
2022-08-09 17:13:07 -0300

    Aula 11 e Aula 12

--- aula11/aula11.py
+++ aula11/aula11.py
@@ -1,5 +1,9 @@
+# Formatando variáveis
 num1 = 14587
 num2 = 1150
 divisao = num1 / num2
 
-print(f'{num1:0>10}')
+nome = 'Gustavo Henrique Paschoal Peralta'
+nome_formatado = '{: >50}'.format(nome)
+
+print(nome_formatado)

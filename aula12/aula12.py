dd5145686fde3708d6d099388c2432b98c41f64b
gustavurzka <gustavo_henrique199@live.com>
2022-08-11 18:00:22 -0300

    Aula 11 e Aula 12

--- aula12/aula12.py
+++ aula12/aula12.py
@@ -1,2 +1,12 @@
 # Manipulando Strings
 text = 'Python S2'
+print(text[8])
+
+url = 'www.google.com.br/'
+print(url[:-1])
+
+nova_string = text[2:6]
+print(nova_string)
+
+nova_string2 = text[0::3]
+print(nova_string2)

fc8195045f38c2976472f802cd1b86988cde8bb7
Gustavo Peralta <gustavo_henrique199@live.com>
2022-08-21 13:12:27 -0300

    Aula 17

--- Bloco1/aula17/aula17.py
+++ Bloco1/aula17/aula17.py
@@ -1,2 +1,15 @@
 # Listas em Python
 
+# lista = ['A', 'B', 'C', 'D', 'E']
+# lista[4] = 'Evo'
+# print(lista[::2])
+
+l1 = list(range(1, 10))
+
+# l1.insert(0, 'b')
+# l1.pop()
+# del(l1[0])
+# print(max(l1))
+# print(min(l1))
+
+

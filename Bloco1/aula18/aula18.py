db54ad5c6497fd05ce0dbf5840ef660ce90ec4c5
gustavurzka <gustavo_henrique199@live.com>
2022-09-09 18:14:28 -0300

    Aula 18

--- Bloco1/aula18/aula18.py
+++ Bloco1/aula18/aula18.py
@@ -4,3 +4,8 @@ For / Else
 
 var = ['Gustavo', 'João', 'Maria']
 
+for valor in var:
+    if valor.startswith('J'):
+        print("Começa com J", valor)
+    else:
+        print('Não começa com J', valor)
